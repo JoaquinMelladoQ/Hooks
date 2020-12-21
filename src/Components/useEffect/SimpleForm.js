696cb2a7909ed6e1a7bb558d85c3adaac95e6433
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-21 19:14:37 -0300

    handleInputChange created, useEffect to console log

--- src/Components/useEffect/SimpleForm.js
+++ src/Components/useEffect/SimpleForm.js
@@ -1,11 +1,41 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import './Effects.css'
 
 export const SimpleForm = () => {
+
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    })
+
+    const { name, email } = formState
+
+    useEffect(() => {
+        console.log('hey')
+    })
+
+    const handleInputChange = (e) => {
+         console.log(e.target)
+    }
+
     return (
         <Fragment>
              <h1>useEffect</h1>
              <hr />   
+
+             <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="from-control"
+                    placeholder="Your name"
+                    autocomplete="off"
+                    value={ name }
+                    onChange={ handleInputChange }
+                />
+             </div>
+
         </Fragment>
     )   
 }
