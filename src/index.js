4ca74c9cb569e12c2ee3e4930510c5a4692b0f44
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-21 15:46:49 -0300

    bootstrap cdn added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HookApp } from './HookApp'
 
 
 ReactDOM.render(
-
-    <App />
-,
+    <HookApp />,
   document.getElementById('root')
 );
 
