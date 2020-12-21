e8e31899b83da05026ae2ffb6c466358cfd4c066
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-21 16:55:26 -0300

    onClick was added with increment and decrement

--- src/Components/useState/CounterWithCustomHook.js
+++ src/Components/useState/CounterWithCustomHook.js
@@ -10,8 +10,8 @@ export const CounterWithCustomHook = () => {
         <Fragment>
             <h1>Counter with hook { state }</h1>
             <hr />
-            <button className="btn btn-primary" > +1 </button>
-            <button className="btn btn-success" > -1 </button>
+            <button onClick={increment} className="btn btn-primary" > +1 </button>
+            <button onClick={decrement} className="btn btn-success" > -1 </button>
         </Fragment>
     )
 }
