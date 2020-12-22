be860adf003b2668460c0073a9c9530f011fd3e1
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 14:41:00 -0300

    calling the endpoint

--- src/Components/Example/MultipleCustomHooks.js
+++ src/Components/Example/MultipleCustomHooks.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useFetch } from '../../Hooks/useFetch'
 import './Example.css'
 
 export const MultipleCustomHooks = () => {
+
+    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`)
+    console.log(state)
+
     return (
         <div>
             <h1>CustomHooks</h1>
