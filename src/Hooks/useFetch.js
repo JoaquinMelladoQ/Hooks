d25b80f8f5ac10c62da2fac8ed5fadfd07032ec5
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 17:39:26 -0300

    incorporate loading when executing action

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -4,7 +4,11 @@ import React, { useState, useEffect } from 'react'
 export const useFetch = ( url ) => {
 
     const [state, setState] = useState({ data: null, loading: true, error: null })
+
     useEffect(() => {
+
+        setState({ data: null, loading: true, error: null })
+
         fetch( url )
             .then( resp => resp.json() )
             .then( data => {
