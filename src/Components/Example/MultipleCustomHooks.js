bdbfd56a57fdc2cd40d0ce3a24710f6e9b683719
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 16:59:55 -0300

    ternary added to main view

--- src/Components/Example/MultipleCustomHooks.js
+++ src/Components/Example/MultipleCustomHooks.js
@@ -4,24 +4,35 @@ import './Example.css'
 
 export const MultipleCustomHooks = () => {
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`)
-    console.log(state)
+    const { loading } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`)
+    // console.log(state)
 
     return (
         <div>
             <h1>Breaking Bad Quotes</h1>
             <hr />
-            <div className="alert alert-info text-center">
-                Loading...
-            </div>
-            <blockquote className="blockquote text-right">
-                <p className="mb-0">
-                    HolaMundo
-                </p>
-                <footer className="blockquote-footer">
-                    Joaco
-                </footer>
-            </blockquote>
+
+            {
+                loading 
+                ?
+                    (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                :
+                    (
+                        <blockquote className="blockquote text-right">
+                            <p className="mb-0">
+                                HolaMundo
+                            </p>
+                            <footer className="blockquote-footer">
+                                Joaco
+                            </footer>
+                        </blockquote>
+                    )
+            }
+
         </div>
     )
 }
