e4fddd6df70ae1d74ab760490bbcf08b3b6a0bfe
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 13:36:58 -0300

    added a custom hook to handle general logic

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import React, { useState } from 'react'
+
+export const useForm = ( initialState = {} ) => {
+    const [values, setValues] = useState(initialState)
+    
+    const handleInputChange = ({ target }) => {
+        setFormState({
+            ...values,
+           [target.name]: target.value
+        })
+    }
+
+    return [ values, handleInputChange ],
+}
