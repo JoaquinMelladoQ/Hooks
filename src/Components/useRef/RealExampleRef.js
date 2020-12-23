5251c2fe0f3922ee01a3619b8e825e8600f3658a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 22:53:44 -0300

    imported MultipleCustomHooks

--- src/Components/useRef/RealExampleRef.js
+++ src/Components/useRef/RealExampleRef.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../useEffect/Effects.css'
+import { MultipleCustomHooks } from '../Example/MultipleCustomHooks'
 
 export const RealExampleRef = () => {
     return (
         <div>
             <h1>RealExampleRef</h1>
+            <hr />
+
+            <MultipleCustomHooks />
         </div>
     )
 }
