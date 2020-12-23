e82f5e4d40a035ae97fe17fca7e4f205e8c2fd95
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 22:56:56 -0300

    useState imported and add a condition that if its true will just render multiplecustomhook

--- src/Components/useRef/RealExampleRef.js
+++ src/Components/useRef/RealExampleRef.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../useEffect/Effects.css'
 import { MultipleCustomHooks } from '../Example/MultipleCustomHooks'
 
 export const RealExampleRef = () => {
+
+    const [show, setShow] = useState(false)
+
     return (
         <div>
             <h1>RealExampleRef</h1>
             <hr />
 
-            <MultipleCustomHooks />
+            { show && <MultipleCustomHooks /> }
         </div>
     )
 }
