1fa82a981b5a363ddcff0eb0557be40da2a3e906
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-22 23:54:56 -0300

    initial settings

--- src/Components/Memos/Memorize.js
+++ src/Components/Memos/Memorize.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useCounter } from '../../Hooks/useCounter'
 import '../useEffect/Effects.css'
 
 
 export const Memorize = () => {
+
+    const { counter, increment } = useCounter( 10 )
+
     return (
         <div>
-            <h1>Memos</h1>
+            <h1>Counter: <small>{ counter }</small></h1>
+            <hr />
+
+            <button className="btn btn-primary"  onClick={ increment } >
+                Add
+            </button>
+
         </div>
     )
 }
