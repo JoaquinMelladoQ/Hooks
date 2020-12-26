2e776adf390e612bb11d97fb344ccabbb6982fb4
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-26 13:56:55 -0300

    using useState and created a counter for example

--- src/Components/Memos/CallBackHook.js
+++ src/Components/Memos/CallBackHook.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../Components/useEffect/Effects.css'
 
 export const CallBackHook = () => {
+
+    const [counter, setCounter] = useState(10)
+    const increment = () => {
+        setCounter( counter + 1 )
+    }
     return (
         <div>
             <h1>CallBack Hook</h1>
+            <hr />
         </div>
     )
 }
