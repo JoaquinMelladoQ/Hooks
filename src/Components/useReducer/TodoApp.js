38152f4c2e07379dd58dd491c4441eff41768d23
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-27 20:24:12 -0300

    adding initial config

--- src/Components/useReducer/TodoApp.js
+++ src/Components/useReducer/TodoApp.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './Styles.css'
 
+const initialState = [{
+    id: new Date().getTime(),
+    desc: 'learn react',
+    done: false
+}]
 
 export const TodoApp = () => {
+
+    const [ state ] = useReducer( reducer, initialState )
+
     return (
         <div>
             <h1>Todo App</h1>
             <hr />
+
+            <ul>
+                <li>Hello</li>
+                <li>World</li>
+                <li>Hello again</li>
+            </ul>
         </div>
     )
 }
